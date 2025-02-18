74cf6e3ab41e23dedb03b29e531efa09d6a6ad0b
Abraham Efraim <75068289+reondaze-a@users.noreply.github.com>
2025-02-18 12:27:34 -0500

    Javascript intialized
    added javascript to the project.

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const initialCards = [
+    (object1 = {
+        name: "Val Thorens",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+    }),
+    (object2 = {
+        name: "Restaurant terrace",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+    }),
+    (object3 = {
+        name: "An outdoor cafe",
+        link: ".https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+    }),
+    (object4 = {
+        name: "A very long bridge, over the forest across the bridge",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+    }),
+    (object5 = {
+        name: "Tunnel with morning light",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+    }),
+    (object6 = {
+        name: "Mountain house",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+    }),
+];
