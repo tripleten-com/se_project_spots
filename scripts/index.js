62ab627fa32a1e31f53853a3def70cda5ee349a4
Abraham Efraim <75068289+reondaze-a@users.noreply.github.com>
2025-02-20 00:26:26 -0500

    Update index.js
    added interactions to edit profile

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,17 @@ const initialCards = [
         link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
     }),
 ];
+
+let modalBlock = document.querySelector(".modal");
+let profileBlock = document.querySelector(".profile");
+let profileEditButton = profileBlock.querySelector(".profile__edit-button");
+let modalCloseButton = modalBlock.querySelector(".modal__close-button");
+
+modalBlock.classList.remove("modal_opened");
+
+function toggleModal() {
+    modalBlock.classList.toggle("modal_opened");
+}
+
+profileEditButton.addEventListener("click", toggleModal);
+modalCloseButton.addEventListener("click", toggleModal);
